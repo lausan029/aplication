6ebd137d7c82daae7b4d98c5c395b13f36aef667
Test git <lau.san029@gmail.com>
2017-06-23 14:27:55 -0400

    other commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'test/home', to: 'test#home'
   get 'test/about', to: 'test#about'
+
+
+  root :to => 'test#home'
+
 end
